e9ca4c675e620838db36b02b450f805e171f0145
farfun <1007530194@qq.com>
2024-01-05 17:14:25 +0800

    update

---
+++ funscript/develop/env.sh
@@ -0,0 +1,3 @@
+pip install -U -i https://pypi.org/simple/ funpypi funbuild
+pip install -U -i https://pypi.org/simple/ funfile funsecret fundrive
+pip install -U -i https://pypi.org/simple/ funread
