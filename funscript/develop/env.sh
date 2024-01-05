71ef016ddc65503bdcc702c4360543b0c4418961
farfun <1007530194@qq.com>
2024-01-05 18:16:29 +0800

    update

--- funscript/develop/env.sh
+++ funscript/develop/env.sh
@@ -1,3 +1,3 @@
 pip install -U -i https://pypi.org/simple/ funpypi funbuild
-pip install -U -i https://pypi.org/simple/ funfile funsecret fundrive
+pip install -U -i https://pypi.org/simple/ funfile funsecret fundrive funtask
 pip install -U -i https://pypi.org/simple/ funread
